790f2cfcfae034422ab8ff7445614aa9687f5869
Michela <michela_mullins@outlook.com>
2025-03-28 22:19:24 +0100

    Initial commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a basic React app with a Home page and an About page.</p>
+    </div>
+  );
+}
+
+export default Home;
