a7f144366c8494e60c1b60340e68fdc08874b90f
Michela <michela_mullins@outlook.com>
2025-04-04 23:54:36 +0200

    Displaying name, id and price

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 const Home = () => {
+    const navigate = useNavigate();
+
     const handleSearch = async (query) => {
-        try {
-            const response = await axios.get(query); // Fetch the page content
-            console.log('Page HTML:', response.data);
-        } catch (error) {
-            console.error('Error fetching the page:', error);
-        }
+        // temporary return data
+        const data = {
+            id: 21342,
+            name: 'The Insect Collection',
+            price: 99.9,
+            img: 'https://www.lego.com/cdn/cs/set/assets/blte32d946cf23b2e5c/21342.png?format=webply&fit=bounds&quality=60&width=800&height=800&dpr=2',
+        };
+
+        navigate(`/sets/${data.id}`, {
+            state: {
+                set: data,
+            },
+        });
     };
 
     return (
