3858b88972b36b9f7d3eaf8ae93b0e2bda0df440
Michela <michela_mullins@outlook.com>
2025-04-08 19:52:33 +0200

    Remove accidental import

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import { useNavigate } from 'react-router';
-import { Typography, Stack, Grid, colors } from '@mui/material';
+import { Typography, Stack, Grid } from '@mui/material';
 import ProductCard from '../components/ProductCard';
 
 const Home = () => {
